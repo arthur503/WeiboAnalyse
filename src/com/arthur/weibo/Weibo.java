48fbfe8fb5ab91978f592f5ac997d06e0e8fbc4e
Arthur King <arthur.dut@gmail.com>
2013-09-12 16:07:52 +0800

    weiboanalyse first commit

---
+++ src/com/arthur/weibo/Weibo.java
@@ -0,0 +1,13 @@
+package com.arthur.weibo;
+
+public class Weibo {
+
+	public Weibo(){
+		System.out.println("Create a new Weibo.");
+	}
+
+	public static void main(String[] argv){
+		Weibo wb = new Weibo();
+	}
+}
+
