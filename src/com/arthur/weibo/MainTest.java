55ab05c5cfb44b178d738d1b1a72da4398bdb74b
Arthur King <arthur.dut@gmail.com>
2013-09-12 16:13:27 +0800

    revise weibo.java

--- src/com/arthur/weibo/MainTest.java
+++ src/com/arthur/weibo/MainTest.java
@@ -3,6 +3,7 @@ package com.arthur.weibo;
 public class MainTest {
 
 	public static void main(String[] argv){
-		Weibo wb = new Weibo();
+		Weibo wb = new Weibo("ouscn");
+		System.out.println(wb.toString());
 	}
 }
--- src/com/arthur/weibo/Weibo.java
+++ src/com/arthur/weibo/Weibo.java
@@ -2,9 +2,15 @@ package com.arthur.weibo;
 
 public class Weibo {
 
-	public Weibo(){
-		System.out.println("Create a new Weibo.");
+	private String name;
+	public Weibo(String name){
+		this.name = name;
 	}
-
+	
+	public String toString(){
+		System.out.print("Create a new Weibo:");
+		return name;
+	}
+	
 }
 
